8686ea97c0bec4e73c19c7a10e56f42b58d6a748
Anthony Brown <abrown4123@gmail.com>
2021-05-24 21:54:31 -0500

    Exercises 2.6 and 2.7

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import People from './components/People'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
@@ -6,23 +7,48 @@ const App = () => {
   ]) 
   const [ newName, setNewName ] = useState('')
 
+  const checkName = person => person.name === newName;
+  
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    const alreadyExists = persons.find(checkName)
+    if (alreadyExists) {
+      setNewName('');
+      return alert(`${newName} already exists`);
+    }
+
+    const newEntry = {
+      name: newName
+    }
+    setPersons(persons.concat(newEntry));
+    setNewName('');
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
-          name: <input />
+          name: <input 
+            value={newName}
+            onChange={handleNameChange}
+          />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map(person => 
+        <People key={person.name} name={person.name} />
+      )}
     </div>
   )
 }
-
-export default App
 
 export default App;
---
+++ phonebook/src/components/People.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const People = (props) => <p>{props.name}</p>
+
+export default People
